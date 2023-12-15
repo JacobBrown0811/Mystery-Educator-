2d03134a89aed92c19e32a4d461321aec4f721c0
Nathan Mange <nmange95@gmail.com>
2023-12-15 14:45:44 -0500

    repo and controller progress

--- backend/src/main/java/org/wecancodeit/backend/controller/HistoryController.java
+++ backend/src/main/java/org/wecancodeit/backend/controller/HistoryController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping() // I think this will have to ping the fact api from apininja
 public class HistoryController {
 
 }
