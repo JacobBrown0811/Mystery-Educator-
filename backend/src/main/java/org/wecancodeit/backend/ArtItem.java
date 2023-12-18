3c461be3b4e46b9a7ef688ca18803d5ce8b1cea4
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-18 10:43:05 -0500

    Began creating ArtItem

---
+++ backend/src/main/java/org/wecancodeit/backend/ArtItem.java
@@ -0,0 +1,9 @@
+package org.wecancodeit.backend;
+
+public class ArtItem {
+    private Long id;
+    private String artist;
+    private String year;
+    private String url;
+    private String classification;
+}
