04cb9052d3764463e506ba5f5c6a448f3840b4e9
zebleya1 <zebleya1@gmail.com>
2023-12-18 11:36:20 -0700

    avoids direct DOM manipulation

--- frontend/src/components/NavBarComponent.jsx
+++ frontend/src/components/NavBarComponent.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+    //Keeps track of the current "Active" section
+    const [activeSection, setActiveSection] = useState(null);
+
+    // Function to update the "Active" section and scroll to the appropriate section
     const navigateTo = (sectionId) => {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
-        }
+        setActiveSection(sectionId);
     };
 
     return (
@@ -13,13 +14,38 @@ const NavBar = () => {
             <div className="navbar">
                 <div className="nav-title">Army Learning App</div>
                 <div className="nav-links">
-                    <button onClick={() => navigateTo("home")}>Home</button>
-                    <button onClick={() => navigateTo("aboutUs")}>About Us</button>
-                    <button onClick={() => navigateTo("contactUs")}>Contact Us</button>
+                    <button
+                        onClick={() => {
+                            navigateTo("home");
+                            // Using scrollIntoView to smoothly scroll to the selected section
+                            document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+                        }}
+                        className={activeSection === "home" ? "active" : ""}
+                    >
+                        Home
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigateTo("aboutUs");
+                            document.getElementById("aboutUs").scrollIntoView({ behavior: "smooth" });
+                        }}
+                        className={activeSection === "aboutUs" ? "active" : ""}
+                    >
+                        About Us
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigateTo("contactUs");
+                            document.getElementById("contactUs").scrollIntoView({ behavior: "smooth" });
+                        }}
+                        className={activeSection === "contactUs" ? "active" : ""}
+                    >
+                        Contact Us
+                    </button>
                 </div>
             </div>
         </nav>
     );
-}
+};
 
 export default NavBar;
