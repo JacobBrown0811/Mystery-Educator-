b7fdb0266d885efe6484543f91dfb64d181b2e24
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-19 10:59:45 -0500

    fixed some imports and tags to make App.js load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from "./components/NavBarComponent";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Layout>
+    <Router>
       <NavBar />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/new-meetups" element={<AboutUs />} />
+          {/* <Route path="/new-meetups" element={<AboutUs />} /> */}
         </Routes>
       </main>
-    </Layout>
+    </Router>
   );
 }
 
