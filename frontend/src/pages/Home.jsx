58d9f2ac6713f4e93db2d98161a9169caf3c87b5
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-19 15:23:29 -0500

    Added Art and History Display components to Home page along with states to prepare for api calls

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import ArtDisplay from "../components/ArtDisplayComponent"
+import { useState, useEffect } from "react";
+
 const Home = () => {
+    const [historyEvent, setHistoryEvent] = useState(null);
+    const [artPiece, setArtPiece] = useState(null);
+
+    // Function to fetch new event and art piece
+    const refreshContent = () => {
+        // Fetch and update state for historyEvent and artPiece
+    };
+
+    useEffect(() => {
+        refreshContent(); // Initial fetch on component mount
+    }, []);
+
     return (
-        <section>
-            
+        <section className="Home">
+            <header>
+                <h1>History and Art Explorer</h1>
+                <p>Discover historical events and art from the same year.</p>
+            </header>
+            <div className="content">
+                <HistoryEvent data={historyEvent} />
+                <ArtDisplay data={artPiece} />
+            </div>
+            <div className="refresh-button">
+                <button onClick={refreshContent}>Refresh</button>
+            </div>
         </section>
     )
 }
