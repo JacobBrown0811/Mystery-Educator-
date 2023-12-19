c675325867217fa02e0cdc9961a407da0ecf0a14
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-19 09:54:08 -0500

    Added javadocs

--- backend/src/main/java/org/wecancodeit/backend/controllers/MetController.java
+++ backend/src/main/java/org/wecancodeit/backend/controllers/MetController.java
@@ -8,6 +8,9 @@ import org.wecancodeit.backend.Services.MetService;
 
 import jakarta.annotation.Resource;
 
+/**
+ * Controller for handling requests related to the Metropolitan Museum of Art.
+ */
 @RestController
 @RequestMapping("/met")
 public class MetController {
@@ -17,7 +20,13 @@ public class MetController {
     public MetController(MetService service) {
         this.service = service;
     }
-
+    
+    /**
+     * Get a random art item from a specific year.
+     * 
+     * @param year The year to filter the art items.
+     * @return ResponseEntity containing the art item or an error message.
+     */
     @GetMapping("/random/{year}")
     public ResponseEntity<?> getRandomObject(@PathVariable String year) throws IOException, InterruptedException {
         return ResponseEntity.ok(service.getItemByYear(year));
--- backend/src/main/java/org/wecancodeit/backend/service/MetService.java
+++ backend/src/main/java/org/wecancodeit/backend/service/MetService.java
@@ -14,42 +14,53 @@ import org.wecancodeit.backend.ArtItem;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Service class for interacting with the Metropolitan Museum of Art API.
+ */
 @Service
 public class MetService {
 
-    public ArtItem getItemByYear(String year) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * Retrieves an art item from a specific year.
+         * 
+         * @param year The year of the art item to retrieve.
+         * @return An ArtItem object containing details of the retrieved art item.
+         */
+        public ArtItem getItemByYear(String year) throws IOException, InterruptedException {
+                HttpClient client = HttpClient.newHttpClient();
+                ObjectMapper objectMapper = new ObjectMapper();
 
-        String url = String.format(
-                "https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=%s&dateEnd=%s&hasImages=true&q=*",
-                year, year);
+                String url = String.format(
+                                "https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=%s&dateEnd=%s&hasImages=true&q=*",
+                                year, year);
 
-        HttpRequest idRequest = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
+                HttpRequest idRequest = HttpRequest.newBuilder()
+                                .uri(URI.create(url))
+                                .GET()
+                                .build();
 
-        HttpResponse<String> idResponse = client.send(idRequest, HttpResponse.BodyHandlers.ofString());
-        Map<String, Object> IDResponseMap = objectMapper.readValue(idResponse.body(), Map.class);
-        List<Integer> ids = (List<Integer>) IDResponseMap.get("objectIDs");
+                HttpResponse<String> idResponse = client.send(idRequest, HttpResponse.BodyHandlers.ofString());
+                Map<String, Object> IDResponseMap = objectMapper.readValue(idResponse.body(), Map.class);
+                List<Integer> ids = (List<Integer>) IDResponseMap.get("objectIDs");
 
-        int randomIndex = new Random().nextInt(ids.size());
-        int randomID = ids.get(randomIndex);
+                int randomIndex = new Random().nextInt(ids.size());
+                int randomID = ids.get(randomIndex);
 
-        HttpRequest objectRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + randomID))
-                .GET()
-                .build();
+                HttpRequest objectRequest = HttpRequest.newBuilder()
+                                .uri(URI.create("https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+                                                + randomID))
+                                .GET()
+                                .build();
 
-        HttpResponse<String> objectResponse = client.send(objectRequest, HttpResponse.BodyHandlers.ofString());
-        Map<String, Object> responseMap = objectMapper.readValue(objectResponse.body(), Map.class);
+                HttpResponse<String> objectResponse = client.send(objectRequest, HttpResponse.BodyHandlers.ofString());
+                Map<String, Object> responseMap = objectMapper.readValue(objectResponse.body(), Map.class);
 
-        String artist = (String) responseMap.get("artistDisplayName");
-        String objectUrl = (String) responseMap.get("objectURL");
-        String classification = (String) responseMap.get("classification");
-        String objectYear = Integer.toString((int) responseMap.get("objectBeginDate"));
+                String artist = (String) responseMap.get("artistDisplayName");
+                String objectUrl = (String) responseMap.get("objectURL");
+                String classification = (String) responseMap.get("classification");
+                String objectYear = Integer.toString((int) responseMap.get("objectBeginDate"));
+                String objectTitle = (String) responseMap.get("title");
 
-        return new ArtItem((long) randomID, artist, objectYear, objectUrl, classification);
-    }
+                return new ArtItem((long) randomID, artist, objectYear, objectUrl, classification, objectTitle);
+        }
 }
