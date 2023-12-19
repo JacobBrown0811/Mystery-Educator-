5435794b82fe0e95a8a703ce9726d36a220c0e81
Nathan Mange <nmange95@gmail.com>
2023-12-19 10:31:15 -0500

    controller done

--- backend/src/main/java/org/wecancodeit/backend/controller/HistoryController.java
+++ backend/src/main/java/org/wecancodeit/backend/controller/HistoryController.java
@@ -2,11 +2,28 @@ package org.wecancodeit.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wecancodeit.backend.service.HistoryFactService;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/history")
 public class HistoryController {
+    @Resource
+    HistoryFactService service;
+
+    @GetMapping("/random/{year}")
+    public ResponseEntity<?> getFactById(@PathVariable String year, HttpServletRequest request) {
+        try {
+            return ResponseEntity.ok(service.getRandomFact(year));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
