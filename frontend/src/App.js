cc8cb7a45ae220b592c908ac6bd0028677ec499c
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-19 10:26:18 -0500

    fixed import mapping after merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import './App.css';
+import NavBar from "./components/NavBarComponent";
+import Home from "./pages/Home";
 
 function App() {
   return (
