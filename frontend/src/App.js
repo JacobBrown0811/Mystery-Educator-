7bb60db07d38b38c54ebedb59cf071ef12a56331
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-19 10:02:14 -0500

    Began laying out home page in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-/* Nav Bar implementation */
-import NavBar from './components/NavBarComponent';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    {/* Nav Bar implementation */}
-    <NavBar />
-    
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello world
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-meetups" element={<AboutUs />} />
+        </Routes>
+      </main>
+    </Layout>
   );
 }
 
