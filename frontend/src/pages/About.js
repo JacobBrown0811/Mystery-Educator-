fff8efb30956fbd26429b115185431f2b35bc28b
Wendy Gonzalez <wgonzalez1103@gmail.com>
2023-12-20 11:11:07 -0500

    updated profile pictures and styling

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -10,7 +10,7 @@ function About() {
       </header>
       <div className="group-container">
         <div className="group-box">
-           <img src={require("./picture/aaron.jpg")} alt="Aaron pfp" className="team-image"/>
+           <img src={require("./Images/aaron.jpg")} alt="Aaron pfp" className="team-image"/>
         <p className="names"> Aaron Zebley </p>
         <a href="https://github.com/Zebleya1" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
           <p>Front-End Development</p>
@@ -20,7 +20,7 @@ function About() {
         </div>
         <div className="group-box">
           
-          <img src={require("./picture/omar.png")} alt="Omar's pfp" className="team-image"/>
+          <img src={require("./Images/omar.png")} alt="Omar's pfp" className="team-image"/>
           <p className="names">Omar Bonnet</p>
           <a href="https://github.com/obonn1" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
           <p>Back-End Development</p>
@@ -29,7 +29,7 @@ function About() {
         </div>
         <div className="group-box">
           {/* quote img */}
-          <img src={require("./picture/nathan.jpg")} alt="nathans pfp" className="team-image"/>
+          <img src={require("./Images/nathan.jpg")} alt="nathans pfp" className="team-image"/>
           <p className="names">Nathan Mange</p>
           <a href="https://github.com/nmange95" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
           <p>Back-End Development</p>
@@ -37,16 +37,17 @@ function About() {
         </div>
         <div className="group-box">
           {/* quote image*/}
-          <img src="placeholder.jpg" alt="Wendy's pfp" className="team-image"/>
+          <img src="https://avatars.githubusercontent.com/u/146746326?v=4" alt="Wendy's pfp" className="team-image"/>
           <p className="names">Wendy Gonzalez</p>
           <a href="https://github.com/GONZ997" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
           <p>Front-End Development</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus rem, iste architecto eius laboriosam magni esse. Sapiente porro quasi ipsam aperiam reiciendis laboriosam consectetur pariatur iusto, et distinctio. Porro perferendis dicta vel velit laborum excepturi ullam earum hic voluptatibus rem?</p>
+          <p>"To make a great dream come true, the first requirement is a great capacity to dream; the second is persistence."</p> 
+          <p> - Cesar Chavez</p>
        
         </div>
         <div className="group-box">
           {/* quotes image */}
-          <img src="placeholder.jpg" alt="Jacob's pfp" className="team-image"/>
+          <img src={require("./Images/jacob.jpg")} alt="Jacob's pfp" className="team-image"/>
           <p className="names">Jacob Brown</p>
           <a href="https://github.com/JacobBrown0811" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
           <p>Front-End Development</p>
