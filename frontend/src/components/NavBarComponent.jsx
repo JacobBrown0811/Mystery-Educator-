42b39483e88c86c635a2596371f6b638a1946c90
zebleya1 <zebleya1@gmail.com>
2023-12-20 10:02:07 -0700

    fixed functions for nav bar

--- frontend/src/components/NavBarComponent.jsx
+++ frontend/src/components/NavBarComponent.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  // Keeps track of the current "Active" section
+  const [activeSection, setActiveSection] = useState(null);
+
+  const handleButtonClick = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-title">History Explorer App</div>
 
       <div className="nav-links">
-        <button>
-          <Link className="home" to="/">Home</Link>
+        <button onClick={() => handleButtonClick("home")}>
+          <Link className={`home ${activeSection === "home" ? "active" : ""}`} to="/">Home</Link>
         </button>
-        <button>
-          <Link className="aboutUs" to="/about">About</Link>
+        <button onClick={() => handleButtonClick("aboutUs")}>
+          <Link className={`aboutUs ${activeSection === "aboutUs" ? "active" : ""}`} to="/about">About</Link>
         </button>
       </div>
-
     </nav>
   );
 };
+
 
 export default NavBar;
