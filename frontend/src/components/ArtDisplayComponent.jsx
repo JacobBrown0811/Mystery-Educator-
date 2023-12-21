972ab52673c1f427b98da929486a4c9c80475829
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-21 09:40:50 -0500

    now handles empty responses from art controller

--- frontend/src/components/ArtDisplayComponent.jsx
+++ frontend/src/components/ArtDisplayComponent.jsx
@@ -3,15 +3,25 @@ function ArtDisplay({ data }) {
         return <p>Loading art...</p>;
     }
 
+    if (data.id === 0) {
+        return <p>Something went wrong when retrieving data from the Metropolitan Museum of Art. Try again or try a different year.</p>;
+    }
+
+    const openArtwork = () => {
+        if (data.url) {
+            window.open(data.url, "_blank", "noopener,noreferrer");
+        }
+    };
+
     return (
         <div className="art-display">
             <h2>{data.title}</h2>
+            <button onClick={openArtwork} className="view-artwork-button">View Artwork</button>
             <p>Artist: {data.artist}</p>
             <p>Year: {data.year}</p>
             {data.classification && <p>Classification: {data.classification}</p>}
-            <a href={data.url} target="_blank" rel="noopener noreferrer">View Artwork</a>
         </div>
     );
 }
 
-export default ArtDisplay
+export default ArtDisplay;
