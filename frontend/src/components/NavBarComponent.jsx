0ec93b443d7a3464aadaf482e3751543b19443c0
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-22 12:23:36 -0500

    pathing and positioning for Army star

--- frontend/src/components/NavBarComponent.jsx
+++ frontend/src/components/NavBarComponent.jsx
@@ -2,27 +2,27 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  // Keeps track of the current "Active" section
-  const [activeSection, setActiveSection] = useState(null);
+    // Keeps track of the current "Active" section
+    const [activeSection, setActiveSection] = useState(null);
 
-  const handleButtonClick = (section) => {
-    setActiveSection(section);
-  };
+    const handleButtonClick = (section) => {
+        setActiveSection(section);
+    };
 
-  return (
-    <nav className="navbar">
-      <div className="nav-title">History Pinboard</div>
+    return (
+        <nav className="navbar">
+            <div className="nav-title">History Pinboard</div>
 
-      <div className="nav-links">
-        <button onClick={() => handleButtonClick("home")}>
-          <Link className={`home ${activeSection === "home" ? "active" : ""}`} to="/">Home</Link>
-        </button>
-        <button onClick={() => handleButtonClick("aboutUs")}>
-          <Link className={`aboutUs ${activeSection === "aboutUs" ? "active" : ""}`} to="/about">About</Link>
-        </button>
-      </div>
-    </nav>
-  );
+            <div className="nav-links">
+                <button onClick={() => handleButtonClick("home")}>
+                    <Link className={`home ${activeSection === "home" ? "active" : ""}`} to="/">Home</Link>
+                </button>
+                <button onClick={() => handleButtonClick("aboutUs")}>
+                    <Link className={`aboutUs ${activeSection === "aboutUs" ? "active" : ""}`} to="/about">About</Link>
+                </button>
+            </div>
+        </nav>
+    );
 };
 
 
